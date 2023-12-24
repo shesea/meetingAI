24950711ba3ca4da1161e7e9bb703056caa376eb
Polina Kurnosova <shesea>
2023-12-24 16:44:47 +0300

    add postgresql connection

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 docker login --username oauth --password ${OAUTH} cr.yandex
 
-docker build . -t cr.yandex/${REGISTRY_ID}/backend:v${VERSION_NUMBER}
+docker build --build-arg cluster_id="${CLUSTER_ID}" --build-arg db_name="${DB_NAME}" \
+ --build-arg db_username="${DB_USERNAME}" --build-arg db_password="${DB_PASSWORD}" \
+  . -t cr.yandex/${REGISTRY_ID}/backend:v${VERSION_NUMBER}
 
 docker push cr.yandex/${REGISTRY_ID}/backend:v${VERSION_NUMBER}
