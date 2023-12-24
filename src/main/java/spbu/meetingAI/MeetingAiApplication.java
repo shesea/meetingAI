03244fab0508e5ad44764ec0d33c3641830e1f42
Polina Kurnosova <shesea>
2023-12-24 21:53:35 +0300

    add cors support

--- src/main/java/spbu/meetingAI/MeetingAiApplication.java
+++ src/main/java/spbu/meetingAI/MeetingAiApplication.java
@@ -2,6 +2,9 @@ package spbu.meetingAI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MeetingAiApplication {
@@ -10,4 +13,13 @@ public class MeetingAiApplication {
 		SpringApplication.run(MeetingAiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
