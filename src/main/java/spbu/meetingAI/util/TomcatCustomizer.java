101f2d9b9d971efa8452be619f58953f576f9f34
Курносова Полина <ukorifey@mail.ru>
2024-05-04 00:01:54 +0300

    remove file size limit

---
+++ src/main/java/spbu/meetingAI/util/TomcatCustomizer.java
@@ -0,0 +1,21 @@
+package spbu.meetingAI.util;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatCustomizer implements
+        WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+
+        factory.addConnectorCustomizers(connector -> {
+            if(connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+    }
+}
